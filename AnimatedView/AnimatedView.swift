cbba071bb9eca0ef35cee61047de5a8042b454ec
sbyka <sbyka@JNETs-Mac-mini.local>
2018-07-26 18:45:26 +0530

    First commit

---
+++ AnimatedView/AnimatedView.swift
@@ -0,0 +1,21 @@
+//
+//  AnimatedView.swift
+//  AnimatedView
+//
+//  Created by sbyka on 26/07/18.
+//  Copyright © 2018 sbyka. All rights reserved.
+//
+
+import UIKit
+
+class AnimatedView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
